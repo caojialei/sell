dd22423a88f529d23a8915ee1cc8620bd9d914a5
123 <123>
2017-04-12 11:03:12 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,52 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods';
+import seller from 'components/seller/seller';
+import ratings from 'components/ratings/ratings';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+// let router = new VueRouter();
+//
+// router.map({
+//   '/goods': {
+//     component: goods
+//   },
+//   '/seller': {
+//     component: seller
+//   },
+//   '/ratings': {
+//     component: ratings
+//   }
+// });
+//
+// router.start(app, '#app');
+
+const routes = [
+  { path: '/', redirect: '/goods' },
+  { path: '/goods', component: goods },
+  { path: '/seller', component: seller },
+  { path: '/ratings', component: ratings }
+];
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+let router = new VueRouter({
+  linkActiveClass: 'active',
+  routes // （缩写）相当于 routes: routes
+});
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
+
